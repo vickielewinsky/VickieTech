300d00b9136c56e79fbfc46571bd77aa359ac49a
unknown <vickielewinsky@gmail.com>
2025-04-10 17:04:07 +0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 // src/pages/_app.tsx
 import Layout from '../components/Layout';
 import '../styles/globals.css'; // Global styles
+import { AppProps } from 'next/app'; // Importing the type for Next.js App props
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
